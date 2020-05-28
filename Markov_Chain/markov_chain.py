6062863bbaa73b7adbcb564d166ffc0f9edf651d
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-05-28 16:43:14 +0530

    Added probabilities and a weighted choice to the markov chain

--- Markov_Chain/markov_chain.py
+++ Markov_Chain/markov_chain.py
@@ -1,5 +1,5 @@
-import numpy as np
 from collections import defaultdict
+import random
 
 
 def get_end_words(words):
@@ -15,19 +15,45 @@ def make_pairs(words):
         yield (words[i], words[i+1])
 
 
-def create_dict(pairs, pair_dict):
-    for word1, word2 in pairs:
-        pair_dict[word1].append(word2)
+def create_list(pairs):
+    pair_list = []
+    for lead, trail in pairs:
+        pair_list.append((lead, trail))
+    return pair_list
+
+
+def check_frequency(words, pair_list, pair_dict):
+    unique_pairs = set(pair_list)
+    for pair in unique_pairs:
+        count_pair = pair_list.count(pair)
+        lead, trail = pair[0], pair[1]
+        count_word = words.count(lead)
+        probability = count_pair/count_word
+        pair_dict[lead].append([trail, probability])
+
     return pair_dict
 
 
+def make_random_choice(trails):
+    if len(trails) == 1:
+        return trails[0][0]
+    else:
+        words = [x for [x, y] in trails]
+        weights = [y for [x, y] in trails]
+        return random.choices(words, weights, k=1)[0]
+
+
 def generate_sentence(words, pair_dict, num_words):
-    first_word = np.random.choice(words)
+    first_word = random.choice(words)
     while first_word.islower() or not first_word.isalpha():
-        first_word = np.random.choice(words)
+        first_word = random.choice(words)
     chain = [first_word]
     for i in range(num_words):
-        chain.append(np.random.choice(pair_dict[chain[-1]]))
+        curr_word = chain[-1]
+        trail_words = pair_dict[curr_word]
+        if not trail_words:
+            trail_words = pair_dict[chain[-2]]
+        chain.append(make_random_choice(trail_words))
     return chain
 
 
@@ -38,8 +64,9 @@ def main():
     pairs = make_pairs(words)
     end_words = get_end_words(words)
     pair_dict = defaultdict(list)
-    pair_dict = create_dict(pairs, pair_dict)
-    num_words = 25
+    pair_list = create_list(pairs)
+    pair_dict = check_frequency(words, pair_list, pair_dict)
+    num_words = 50
     word_gen = generate_sentence(words, pair_dict, num_words)
     if word_gen[-1] not in end_words:
         word_gen.append(np.random.choice(end_words))
